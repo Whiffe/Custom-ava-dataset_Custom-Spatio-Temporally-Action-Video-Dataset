1863814c7ba678f50d128e244aec93523fd88799
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-04-23 08:36:01 +0800

    Update cut_video.sh

--- Dataset/cut_video.sh
+++ Dataset/cut_video.sh
@@ -1,5 +1,5 @@
-IN_DATA_DIR="/home/Dataset/videos"
-OUT_DATA_DIR="/home/Dataset/video_crop"
+IN_DATA_DIR="/home/Custom-ava-dataset_Custom-Spatio-Temporally-Action-Video-Dataset/Dataset/videos"
+OUT_DATA_DIR="/home/Custom-ava-dataset_Custom-Spatio-Temporally-Action-Video-Dataset/Dataset/video_crop"
 ffmpeg -ss 1030 -t 11 -y -i "${IN_DATA_DIR}/1.mp4" "${OUT_DATA_DIR}/1.mp4"
 ffmpeg -ss 1340 -t 11 -y -i "${IN_DATA_DIR}/1.mp4" "${OUT_DATA_DIR}/2.mp4"
 ffmpeg -ss 1850 -t 11 -y -i "${IN_DATA_DIR}/1.mp4" "${OUT_DATA_DIR}/3.mp4"
