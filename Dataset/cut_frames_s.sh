97e8c16c87e1e4279759da2ca9cd849d437a6573
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-05-10 13:26:42 +0800

    Create cut_frames_s.sh

---
+++ Dataset/cut_frames_s.sh
@@ -0,0 +1,25 @@
+IN_DATA_DIR="/user-data/studentVideo/clips"
+OUT_DATA_DIR="./frames"
+
+if [[ ! -d "${OUT_DATA_DIR}" ]]; then
+  echo "${OUT_DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${OUT_DATA_DIR}
+fi
+
+for video in $(ls -A1 -U ${IN_DATA_DIR}/*)
+do
+  video_name=${video##*/}
+
+  echo $video_name
+  array=(${video_name//./ })
+  video_name=${array[0]}
+  echo $video_name
+    
+
+  out_video_dir=${OUT_DATA_DIR}/${video_name}/
+  mkdir -p "${out_video_dir}"
+
+  out_name="${out_video_dir}/${video_name}_%06d.jpg"
+
+  ffmpeg -i "${video}" -r 30 -q:v 1 "${out_name}"
+done
