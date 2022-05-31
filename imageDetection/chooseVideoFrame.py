7b882d22a5260eb7744c6aac4c79bc135ee72531
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-05-31 20:02:56 +0800

    Update chooseVideoFrame.py

--- imageDetection/chooseVideoFrame.py
+++ imageDetection/chooseVideoFrame.py
@@ -1,22 +1,20 @@
 import os
 import shutil
 import sys
+import argparse
 
-#这里输入视频有多少秒
-#seconds = 180
-#传参 这里传入视频多少秒
-seconds = int(sys.argv[1])
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--seconds', default=15,type=int, help="Length of video, seconds")
+parser.add_argument('--frames_dir', default='../Dataset/frames',type=str, help="The path of all video frames")
+
+arg = parser.parse_args()
 
 #选取视频中间的一帧
-chooseSecond = int(seconds/2)
-
-# 默认从第二秒开始检测标注
-# start = 2
-#传参 这里传入视频从那一秒开始，这里需要设置为 0
-start = int(sys.argv[2])
+chooseSecond = int(arg.seconds/2)
 
 # 需要检测标注的时间位置[0,1,2,3,4,5,6,7,8,9,10]
-frames = range(start, seconds+1)
+frames = range(0, arg.seconds+1)
 
 # num_frames 存放对应图片的编号
 num_frames = []
@@ -24,7 +22,7 @@ num_frames = []
 for i in frames:
     num_frames.append(i*30+1)
 #遍历./frames
-for filepath,dirnames,filenames in os.walk(r'../Dataset/frames'):
+for filepath,dirnames,filenames in os.walk(arg.frames_dir):
     filenames=sorted(filenames)
     for filename in filenames:
         if str(chooseSecond*30+1) in filename:
