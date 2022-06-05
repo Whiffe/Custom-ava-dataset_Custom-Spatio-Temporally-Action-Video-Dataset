fd16ad2e0d660d204bccc5d0363ad320f7e7c488
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-06-05 14:07:31 +0800

    Update makePretrainModel.py

--- yolovDeepsort/yolov5/makePretrainModel.py
+++ yolovDeepsort/yolov5/makePretrainModel.py
@@ -1,7 +1,15 @@
 import torch
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--weight_dir', default='/user-data/yolov5File/crowdhuman_vbody_yolov5m.pt',type=str, help="the path of crowdhuman_vbody_yolov5m.pt")
+parser.add_argument('--preWeight_dir', default='../../imageDetection/pretainModel/crowdhuman_vbody_yolov5m_pretrained.pt',type=str, help="the path of crowdhuman_vbody_yolov5m_pretrained.pt")
+
+arg = parser.parse_args()
 
 # 默认加载
-net = torch.load('/user-data/yolov5File/crowdhuman_vbody_yolov5m.pt')
+net = torch.load(arg.weight_dir)
 # 用CPU加载
 # net = torch.load('model_final.pth',map_location=torch.device('cpu'))
 
@@ -10,4 +18,4 @@ for k in net.keys():
     print(k)
 
 net_only_model = {"model": net["model"],"optimizer": net["optimizer"],"epoch": net["epoch"]}
-torch.save(net_only_model, '../../imageDatasetDetection/pretainModel/crowdhuman_vbody_yolov5m_pretrained.pt')
+torch.save(net_only_model, arg.preWeight_dir)
